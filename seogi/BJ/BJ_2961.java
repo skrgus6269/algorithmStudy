0b84beb7c76d19d4247849538816fb35be025a27
김서기 <seogi9898@gmail.com>
2024-06-20 00:22:35 +0900

    #7 도영이가 만든 맛있는 음식

---
+++ seogi/BJ/BJ_2961.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/*
+ * https://www.acmicpc.net/source/79776832
+ * java8 / 131876 KB / 760 ms /
+ */
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw=  new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st = new StringTokenizer(bf.readLine());
+    StringBuilder sb = new StringBuilder();
+    int N = Integer.parseInt(st.nextToken());
+    int arr[][] = new int[N][2];
+    for (int i = 0; i < N; i++) {
+
+    }
+  }
+}
