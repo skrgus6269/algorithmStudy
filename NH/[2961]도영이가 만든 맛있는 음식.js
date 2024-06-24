0ce63310957c2b672b4eb2fa65c028bda6d3e64c
skrgus6269 <knh6269@gmail.com>
2024-06-24 22:04:59 +0900

    도영이가 만든 음식 제출

--- NH/[2961]도영이가 만든 맛있는 음식.js
+++ NH/[2961]도영이가 만든 맛있는 음식.js
@@ -1,40 +1,36 @@
-/*
-언어: javascript
-사용한 알고리즘: dfs
-1차 시도 : 메모리 초과 
-node.js로는 풀 수 없다네요..
-*/
-// let input = require('fs').readFileSync('예제.txt').toString().trim().split('\r\n');
+/* 
+알고리즘: 브루트 포스를 활용
+*/ 
 let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const length = Number(input[0]);
+const N = input.splice(0,1)[0];
 
-//소수 검증 
-const checkPrime = (number) => { 
-    if (number === 1) return false;
+const recipe = input.map((item) => item.split(' ').map(Number))
+ let min = 999999999;
 
-    for (let i = 2; i < Math.sqrt(number); i++) {
-        if (number % i === 0) {
-            return false;
-        }
-    }
-    return true;
-} 
-
-const dfs = (number) => { 
-    if (String(number).length === length) {
-        console.log(number);
-        return ;
-    }
-    //2번째 자리부터는 1,3,7,9 만 올 수 있음
-    if (checkPrime(Number(String(number)  + '1'))) dfs(Number(String(number)  + '1'))  
-    if (checkPrime(Number(String(number)  + '3'))) dfs(Number(String(number)  + '3'))  
-    if (checkPrime(Number(String(number)  + '7'))) dfs(Number(String(number)  + '7')) 
-    if (checkPrime(Number(String(number)  + '9'))) dfs(Number(String(number)  + '9')) 
+const calculate = (array) => {
+    let sum = 0;
+    let mul = 1; 
+    array.forEach((item) => {
+        const [a, b] = recipe[item];
+        sum += b;
+        mul *= a;
+    }) 
+    min = Math.min(Math.abs(sum - mul), min) 
 }
- 
-//첫자리는 소수인 2,3,5,7만 올 수 있음
-dfs(2) 
-dfs(3) 
-dfs(5) 
-dfs(7)   
+
+const dfs = (index, array) => {
+    calculate(array);
+    if (array.length === N) return ;
+    for (let i = index + 1; i < N; i++) {
+        dfs(i ,[...array, i]);
+        dfs(i , array);
+    }
+}
+
+for (let i = 0; i <= N - 1; i++) {
+    dfs(i, [i])
+}
+
+console.log(min)
+     
